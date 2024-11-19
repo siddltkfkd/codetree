6c00c8e93482d4d69df1c04da7a6d8e8bb8189e0
이은지 <siddltkfkd213@gmail.com>
2024-11-19 20:49:21 +0900

    Title: 별 찍기 1, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241119/별 찍기 1/star-make-1.java
+++ 241119/별 찍기 1/star-make-1.java
@@ -25,15 +25,14 @@ public class Main {
             }
         }
         else if (format == 3) {
-            int star = 1;
-            for (int i=1; i<=n; i++) {
-                for (int j=0; j<n-i; j++) {
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<n-i-1; j++) {
                     sb.append(" ");
                 }
-                for (int j=0; j<star; j++) {
+                for (int j=0; j<i*2; j++) {
                     sb.append("*");
-                    star += 2;
                 }
+                sb.append("*");
                 for (int j=0; j<n-i; j++) {
                     sb.append(" ");
                 }
