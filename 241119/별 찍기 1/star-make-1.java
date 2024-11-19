418c3eff500681c714445032f3d7c1cdc2cb7bc2
이은지 <siddltkfkd213@gmail.com>
2024-11-19 20:42:23 +0900

    Title: 별 찍기 1, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241119/별 찍기 1/star-make-1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int format = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        if (format == 1) {
+            for (int i=1; i<=n; i++) {
+                for (int j=0; j<i; j++) {
+                    sb.append("*");
+                }
+                sb.append("\n");
+            }
+        }
+        else if (format == 2) {
+            for (int i=n; i>0; i--) {
+                for (int j=0; j<i; j++) {
+                    sb.append("*");
+                }
+                sb.append("\n");
+            }
+        }
+        else if (format == 3) {
+            for (int i=1; i<=n; i++) {
+                for (int j=0; j<n-i; j++) {
+                    sb.append(" ");
+                }
+                for (int j=0; j<i; j++) {
+                    sb.append("*");
+                }
+                for (int j=0; j<n-i; j++) {
+                    sb.append(" ");
+                }
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
